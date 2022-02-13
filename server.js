7455c7b3ec4ca895e5b3217122f7d6636e533789
CraigJensen <cmjensen82@gmail.com>
2022-02-12 19:55:03 -0600

    localhost working, live site not working

--- server.js
+++ server.js
@@ -22,14 +22,15 @@ const sess = {
   }),
 };
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.use(session(sess));
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
 // turn on routes
 app.use(routes);
 
