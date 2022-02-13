bb5dccc8f550e40725ab5f38afaa99ef4bcbad5f
CraigJensen <cmjensen82@gmail.com>
2022-02-13 07:35:38 -0600

    maybe fixed the deployment issue

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ const exphbs = require('express-handlebars');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const sequelize = require('./config/connection.js');
+const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
-  secret: process.env.SESS_PW,
+  secret: secretSessionShh,
   cookie: {},
   resave: false,
   saveUninitialized: true,
@@ -22,7 +22,6 @@ const sess = {
 app.use(session(sess));
 
 const helpers = require('./utils/helpers');
-
 const hbs = exphbs.create({ helpers });
 
 app.engine('handlebars', hbs.engine);
