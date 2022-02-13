6d355163c5443eac3aa2e8a3b849f5ca26dea85a
CraigJensen <cmjensen82@gmail.com>
2022-02-13 07:04:13 -0600

    still working on deployed app

--- server.js
+++ server.js
@@ -1,15 +1,12 @@
 const express = require('express');
-const routes = require('./controllers');
-const sequelize = require('./config/connection.js');
-const path = require('path');
-const helpers = require('./utils/helpers');
-const exphbs = require('express-handlebars');
-const hbs = exphbs.create({ helpers });
 const session = require('express-session');
+const path = require('path');
+const exphbs = require('express-handlebars');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const sequelize = require('./config/connection.js');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
@@ -23,6 +20,9 @@ const sess = {
 };
 app.use(session(sess));
 
+const helpers = require('./utils/helpers');
+const hbs = exphbs.create({ helpers });
+
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
@@ -31,7 +31,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // turn on routes
-app.use(routes);
+app.use(require('./controllers/'));
 
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
